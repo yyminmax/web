63badc5b345ecea04c4965843f516d6815c21a50
yyminmax <yyminmax@gmail.com>
2016-01-06 23:46:11 +0800

    some changes

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, url_for, session, redirect
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	#name = None
+	form = NameForm()
+	if(form.validate_on_submit()):
+		old_name = session.get('name')
+		if(old_name is not None and old_name != form.name.data):
+			flash('Maybe you have a new name.')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
